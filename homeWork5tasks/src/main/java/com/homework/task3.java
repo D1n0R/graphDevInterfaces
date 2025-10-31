45e2ed31f382ab0670cf4b5b2dbb258bc5a05ef2
D1n0R <d1n0r1788@gmail.com>
2025-10-31 17:30:09 +0300

    3 task done

--- homeWork5tasks/src/main/java/com/homework/task3.java
+++ homeWork5tasks/src/main/java/com/homework/task3.java
@@ -1,4 +1,24 @@
 package com.homework;
 
 public class task3 {
+    private static String funcStrings(String strPref, String strSuf){
+        if(strSuf.isEmpty())
+            return strPref;
+
+        String newStrPref;
+        if(strSuf.charAt(0) == '#'){
+            if(!strPref.isEmpty())
+                newStrPref = strPref.substring(0, strPref.length()-1);
+            else
+                newStrPref = "";
+        }
+        else{
+            newStrPref = strPref + strSuf.charAt(0);
+        }
+        return funcStrings(newStrPref, strSuf.substring(1));
+    }
+
+    public static boolean compareStrings(String string1, String string2){
+        return funcStrings("",string1).equals(funcStrings("", string2));
+    }
 }
