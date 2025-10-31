d9f1e62d5be9bb585ebb72229571523e1825026f
D1n0R <d1n0r1788@gmail.com>
2025-10-31 17:43:09 +0300

    task 4 is done

--- homeWork5tasks/src/main/java/com/homework/task4.java
+++ homeWork5tasks/src/main/java/com/homework/task4.java
@@ -1,4 +1,34 @@
 package com.homework;
 
+import java.util.*;
+
 public class task4 {
+
+    private static boolean binSearch(List<Integer> list, int target) {
+        int l = 0;
+        int r = list.size() - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (list.get(m) == target) {
+                return true;
+            } else if (list.get(m) < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static List<Integer> findMissing(List<Integer> list1, List<Integer> list2) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : list1)
+            if (!binSearch(list2, num)) result.add(num);
+
+        return result;
+    }
 }
