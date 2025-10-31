6837d2501839118d3a0e1ac47c545c32314d2be8
D1n0R <d1n0r1788@gmail.com>
2025-10-31 18:56:33 +0300

    Tests for tasks

--- homeWork5tasks/src/main/java/com/homework/Main.java
+++ homeWork5tasks/src/main/java/com/homework/Main.java
@@ -1,7 +1,26 @@
 package com.homework;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(task2.calcChildWidths(200, Arrays.asList(-1, -1, -2))); // [50.0, 50.0, 100.0]
+        System.out.println(task2.calcChildWidths(100, Arrays.asList(30, 40)));     // [30.0, 40.0]
+        System.out.println(task2.calcChildWidths(50, Arrays.asList(60, -1)));      // !!!!!!!!!!
+
+        System.out.println(task3.compareStrings("ab#c", "ad#c"));      // true
+        System.out.println(task3.compareStrings("ab##", "c#d#"));      // true
+        System.out.println(task3.compareStrings("a#c", "b"));          // false
+        System.out.println(task3.compareStrings("######abc", "#abc")); // true
+
+        System.out.println(task4.findMissing(Arrays.asList(1, 2, 3), Arrays.asList(3, 4)));      // [1, 2]
+        System.out.println(task4.findMissing(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3)));   // []
+        System.out.println(task4.findMissing(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6)));   // [1, 2, 3]
+
+        System.out.println(task5.findLongestMonotonic(Arrays.asList(2, 7, 5, 4, 4, 3)));  // (1, 3)
+        System.out.println(task5.findLongestMonotonic(Arrays.asList(1, 1)));              // (1, 1)
+        System.out.println(task5.findLongestMonotonic(Arrays.asList(5, 4, 3, 2, 1, 2)));  // (0, 4)
+        
     }
 }
